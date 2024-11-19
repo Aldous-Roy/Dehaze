f042bddb52cd9cadbc269b30516349e82a2f9f5e
Aldous Roy <aldousory007@gmail.com>
2024-11-19 18:54:11 +0530

    tailwindcss integeration

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div>
-      <h1>Hello World</h1>
+      <h1 className='text-3xl text-center'>Hello World</h1>
     </div>
     </>
   )
